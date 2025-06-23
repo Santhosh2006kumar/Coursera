3a0e07518b0104a1fe951a19badad587bc0aa675
Santhosh2006kumar <ssksaranathancollege@gmail.com>
2025-06-23 20:43:58 +0530

    Add files via upload
    Signed-off-by: Santhosh2006kumar <ssksaranathancollege@gmail.com>

---
+++ javascript course/ex2/control_flow.js
@@ -0,0 +1,8 @@
+let user = "admin";
+if (user === "admin"){
+    console.log('Grant Full Access');
+}else if (user === "manager"){
+    console.log("Grant Limited Access");
+}else{
+    console.log('Grant No Access');
+}
