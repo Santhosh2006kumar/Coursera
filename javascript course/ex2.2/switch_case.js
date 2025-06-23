ba1bc5ead292bfd825e8a19aac152b6da78f2014
Santhosh2006kumar <ssksaranathancollege@gmail.com>
2025-06-23 21:06:34 +0530

    Add files via upload
    Signed-off-by: Santhosh2006kumar <ssksaranathancollege@gmail.com>

---
+++ javascript course/ex2.2/switch_case.js
@@ -0,0 +1,14 @@
+let user ="Subscriber";
+switch(user){
+    case "admin":
+        console.log("userCategory:Administrator");
+        break;
+    case "manager":
+        console.log("userCategory:Manager");
+        break;
+    case "Subscriber":
+        console.log("userCategory:Subscriber");
+        break;
+    default:
+        console.log("userCategory:Employee");
+}
